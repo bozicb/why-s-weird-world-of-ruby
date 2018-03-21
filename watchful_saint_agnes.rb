05a792801901e6b482ea75ff8a97707058bd6e82
Bojan Božić <boj.bozic@gmail.com>
2018-03-21 22:52:19 +0000

    new

---
+++ watchful_saint_agnes.rb
@@ -0,0 +1,13 @@
+# See, here is the module -- where else could our code possibly stay?
+module WatchfulSaintAgnes
+
+  # A CONSTANT is laying here by the doorway.  Fine.
+  TOOTHLESS_MAN_WITH_FORK = ['man', 'fork', 'exposed gums']
+
+  # A Class is eating, living well in the kitchen.
+  class FatWaxyChild; end
+
+  # A Method is hiding back in the banana closet, God knows why.
+  def timid_foxfaced_girl; {'please' => 'i want an acorn please'}; end
+
+end
