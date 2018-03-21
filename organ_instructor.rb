3c1182c900c6198561e9c481a559f9d2461c9142
Bojan Božić <boj.bozic@gmail.com>
2018-03-21 20:18:34 +0000

    update

--- organ_instructor.rb
+++ organ_instructor.rb
@@ -10,8 +10,16 @@ def wipe_mutterings_from( sentence )
     sentence[open..close] = '' if close
   end
   sentence
+  # better alternative: sentence.gsub(/\([-\w]+\)/,'')
 end
 
 what_he_said = "But, strangely (em-pithy-dah),
   I learned upon, played upon (pon-shoo) the 
   organs on my home (oth-rea) planet."
+
+str = "A string is a long shelf of letters and spaces."
+puts str[0]       # prints 'A'
+puts str[0..-1]   # prints 'A string is a long shelf of letters and spaces.'
+puts str[1..-2]   # prints ' string is a long shelf of letters and spaces'
+puts str[0, 3]    # prints 'A s'
+puts str['shelf'] # prints 'shelf'
