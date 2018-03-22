3902b0f63ae1924188c61c10ba6de4b47c8c4db9
Bojan Božić <boj.bozic@gmail.com>
2018-03-22 11:47:31 +0000

    update

--- lottery-ticket.rb
+++ lottery-ticket.rb
@@ -1,15 +1,24 @@
 class LotteryTicket
-  NUMERIC_RANGE = 1..25
-  attr_reader :picks, :purchased
-  def initialize( *picks )
-    if picks.length != 3
-      raise ArgumentError, "three numbers must be picked"
-    elsif picks.uniq.length != 3
-      raise ArgumentError, "the three picks must be different numbers"
-    elsif picks.detect { |p| not NUMERIC_RANGE === p }
-      raise ArgumentError, "the three picks must be numbers between 1 and 25"
+  NUMERIC_RANGE=1..25
+  attr_reader :picks,:purchased
+  def initialize(*picks)
+    if picks.length!=3
+      raise ArgumentError,"three numbers must be picked"
+    elsif picks.uniq.length!=3
+      raise ArgumentError,"the three picks must be different numbers"
+    elsif picks.detect{|p| not NUMERIC_RANGE === p}
+      raise ArgumentError,"the three picks must be numbers between 1 and 25"
     end
-    @picks = picks
-    @purchased = Time.now
+    @picks=picks
+    @purchased=Time.now
+  end
+  def self.new_random
+    new(rand(25)+1,rand(25)+1,rand(25)+1)
+  rescue ArgumentError
+    retry
   end
 end
+
+ticket=LotteryTicket.new(rand(25)+1,rand(25)+1,rand(25)+1)
+p ticket.picks
+p ticket.purchased
