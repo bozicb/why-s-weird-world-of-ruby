8b026fabb6cd6ecc80dc9f0449a350afcd16529e
Bojan Božić <boj.bozic@gmail.com>
2018-03-22 12:40:56 +0000

    update

--- lottery-draw.rb
+++ lottery-draw.rb
@@ -13,4 +13,26 @@ LotteryDraw.buy 'Yal-dal-rip-sip',
                 LotteryTicket.new(5,1,3),
                 LotteryTicket.new(24,6,8)
 
+class << LotteryDraw
+  def play
+    final=LotteryTicket.new_random
+    winners={}
+    @@tickets.each do |buyer,ticket_list|
+      ticket_list.each do |ticket|
+        score=ticket.score(final)
+        next if score.zero?
+        winners[buyer]||=[]
+        winners[buyer]<<[ticket,score]
+      end
+    end
+    @@tickets.clear
+    winners
+  end
+end
 
+LotteryDraw.play.each do |winner,tickets|
+  puts winner+'won on '+tickets.length+' ticket(s)!'
+  tickets.each do |ticket,score|
+    puts "\t"+ticket.picks.join(', ')+": "+score
+  end
+end
